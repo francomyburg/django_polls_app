1c7369c726d6b453e8b4e27a0fb521c243b391b8
myburgfranco <myburgfranco@gmail.com>
2023-09-13 17:09:43 -0300

    tercer commit primer template creado

--- premiosPlatzi/polls/views.py
+++ premiosPlatzi/polls/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Question
+
 # Create your views here.
 
+#index es una function based view
 def index(request):
-    return HttpResponse("hello world")
+    latest_question_list = Question.objects.all()
+    return render(request,"polls/index.html", {
+        "latest_question_list":latest_question_list
+    })
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta N° {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"<h1>Estas votando a la pregunta N° {question_id} </h1>")
+
